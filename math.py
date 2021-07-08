7049e72f3d5fb1ca06fd1613ef73890664f169a5
Shweta Chindhe <56247789+Shweta2899@users.noreply.github.com>
2021-07-08 14:31:03 +0530

    First file added

---
+++ math.py
@@ -0,0 +1,15 @@
+# Add implementation
+def add(x,y):
+    pass
+    
+#subtract
+def subtract(x,y):
+    pass
+    
+#multiply
+def multiply(x,y):
+    pass
+    
+#divide implementatioin
+def divide(x,y):
+    pass
